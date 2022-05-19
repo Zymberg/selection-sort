6960ac1c3642f760671616412fc97379936dd1f4
Granit Zymeri <gzymeri55@gmail.com>
2022-05-19 17:16:04 -0400

    done

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,5 +1,23 @@
 function selectionSort(arr) {
   // type your code here
+
+        
+  for(let i = 0; i < arr.length; i++) {
+      // Finding the smallest number in the subarray
+      let min = i;
+      for(let j = i+1; j < arr.length; j++){
+          if(arr[j] < arr[min]) {
+              min=j; 
+          }
+       }
+       if (min != i) {
+           // Swapping the elements
+           let tmp = arr[i]; 
+           arr[i] = arr[min];
+           arr[min] = tmp;      
+      }
+  }
+  return arr;
 }
 
 if (require.main === module) {
